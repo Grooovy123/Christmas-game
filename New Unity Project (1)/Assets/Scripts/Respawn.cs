eec6b8f7c95df316f73542df4ffa473e92559d95
Sachta291 <476736@chrome.escg.ac.uk>
2019-12-19 14:05:06 +0000

    unity game

---
+++ New Unity Project (1)/Assets/Scripts/Respawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private Transform PlayerTransform;
+    private Transform PlayerTransformTwo;
+
+    public Transform spawnPoint;
+    public Transform spawnPointTwo;
+    void Start()
+    {
+        PlayerTransform = GameObject.Find("playerTest").transform;
+        PlayerTransformTwo = GameObject.Find("playerTestTwo").transform;
+    }
+    void OnTriggerEnter()
+    {
+        if (PlayerTransform.position.y <= -4)
+        {
+            PlayerTransform.position = spawnPoint.position;
+        }
+
+        if (PlayerTransformTwo.position.y <= -4)
+        {
+            PlayerTransformTwo.position = spawnPointTwo.position;
+        }
+    }
+}
